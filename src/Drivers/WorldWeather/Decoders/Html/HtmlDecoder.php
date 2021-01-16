b98b55ec9596673ecbd46722825dfa5fef521490
ArtARTs36 <temicska99@mail.ru>
2021-01-16 15:22:24 +0300

    init

---
+++ src/Drivers/WorldWeather/Decoders/Html/HtmlDecoder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ArtARTs36\WeatherArchive\Drivers\WorldWeather\Decoders\Html;
+
+use ArtARTs36\WeatherArchive\Contracts\TypeCasterInterface;
+use ArtARTs36\WeatherArchive\Drivers\GisMeteo\Decoders\Html\CyrillicDomDocument;
+use ArtARTs36\WeatherArchive\Drivers\GisMeteo\Decoders\Html\DecodeMachine;
+use ArtARTs36\WeatherArchive\Drivers\GisMeteo\Decoders\Html\DomNodeField;
+
+class HtmlDecoder
+{
+    protected $domDocument;
+
+    protected $columnsMap;
+
+    protected $decodeMachine;
+
+    public function __construct(DecodeMachine $machine, CyrillicDomDocument $domDocument)
+    {
+        $this->decodeMachine = $machine;
+        $this->domDocument = $domDocument;
+        $this->columnsMap = $this->createColumnsMap();
+    }
+
+    public function decode(string $response)
+    {
+        $this->domDocument->loadHTML($response);
+
+        /** @var \DOMNodeList|\DOMDocument[] $rows */
+        $rows = $this
+            ->domDocument
+            ->getElementsByTagName('ul')
+            ->item(3)
+            ->getElementsByTagName('li');
+
+        $values = [];
+
+        foreach ($rows as $rowId => $row) {
+            if (empty($row->nodeValue)) {
+                continue;
+            }
+
+            /**
+             * @var \DOMElement $child
+             */
+            foreach ($row->childNodes as $childId => $child) {
+                if (! isset($this->columnsMap[$childId])) {
+                    continue;
+                }
+
+                $values[$rowId][$this->columnsMap[$childId]->name] = $this->decodeMachine->prepareField(
+                    $this->columnsMap[$childId],
+                    $child
+                );
+            }
+        }
+
+        return $values;
+    }
+
+    protected function createColumnsMap(): array
+    {
+        return [
+            0 => new DomNodeField('day', TypeCasterInterface::TYPE_INT),
+            1 => null,
+            2 => new DomNodeField('temperature', TypeCasterInterface::TYPE_INT),
+        ];
+    }
+}
---
+++ tests/WorldWeatherParserDriverTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ArtARTs36\WeatherArchive\Tests;
+
+use ArtARTs36\WeatherArchive\Contracts\Place;
+use ArtARTs36\WeatherArchive\Contracts\UrlCreator;
+use ArtARTs36\WeatherArchive\Drivers\GisMeteo\Decoders\Html\CyrillicDomDocument;
+use ArtARTs36\WeatherArchive\Drivers\GisMeteo\Decoders\Html\DecodeMachine;
+use ArtARTs36\WeatherArchive\Drivers\WorldWeather\Decoders\Html\HtmlDecoder;
+use ArtARTs36\WeatherArchive\Drivers\WorldWeather\WorldWeatherParserDriver;
+use ArtARTs36\WeatherArchive\Drivers\WorldWeather\WorldWeatherUrlCreator;
+use ArtARTs36\WeatherArchive\EntityCreator;
+use ArtARTs36\WeatherArchive\Support\TypeCaster;
+use PHPUnit\Framework\TestCase;
+
+final class WorldWeatherParserDriverTest extends TestCase
+{
+    public function testGetOnMonth(): void
+    {
+        $driver = $this->createInstance(file_get_contents(__DIR__ . '/mock/world_weather_response.html'));
+
+        $days = $driver->getOnMonth(new \DateTime(), new \ArtARTs36\WeatherArchive\Entities\Place('russia/voronezh'));
+
+        self::assertCount(30, $days);
+    }
+
+    protected function createInstance(string $response)
+    {
+        return new class(
+            new HtmlDecoder(new DecodeMachine(new TypeCaster()), new CyrillicDomDocument()),
+            new EntityCreator(),
+            new WorldWeatherUrlCreator(),
+            $response
+        ) extends WorldWeatherParserDriver {
+            private $response;
+
+            public function __construct(
+                HtmlDecoder $decoder,
+                EntityCreator $entityCreator,
+                UrlCreator $urlCreator,
+                string $response
+            ) {
+                parent::__construct($decoder, $entityCreator, $urlCreator);
+
+                $this->response = $response;
+            }
+
+            protected function getContent(\DateTimeInterface $time, Place $place): string
+            {
+                return $this->response;
+            }
+        };
+    }
+}
